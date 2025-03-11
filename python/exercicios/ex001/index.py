fd016984b36ed9dcea82564fa1f3977c747259cc
Olavo Neves <olavo9neves@gmail.com>
2025-03-11 12:37:17 -0300

    py

---
+++ python/exercicios/ex001/index.py
@@ -0,0 +1,12 @@
+name = input('Digite seu nome: ')
+print('Bem vindo ao melhor lugar da internet ' + name)
+
+diaNascimento = int(input('Em que dia você nasceu? '))
+mesNascimento = input('Em que mês você nasceu? ')
+anoNascimento = int(input('Em qual ano você nasceu? '))
+
+print('Então você nasceu no dia ', diaNascimento, 'de ', mesNascimento, 'de ', anoNascimento, ', Certo?')
+
+firstNumber = int(input('Digite o primeiro número: '))
+secondNumber = int(input('Digite o segundo número: '))
+print('A soma é ', firstNumber + secondNumber)
