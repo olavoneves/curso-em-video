9718dbfdc9254fa8f0d0ba55f67c416d8fce1a1c
Olavo Neves <olavo9neves@gmail.com>
2025-03-12 15:48:07 -0300

    desafio

---
+++ python/desafios/desafio002.py
@@ -0,0 +1,6 @@
+n = input('Digite algo: ')
+print(n.isnumeric()) # numero
+print(n.isalpha()) # letras
+print(n.isalnum()) # numero e letra
+print(n.isupper()) # maiusculo
+print(n.islower()) # minusculo
