0167b11961e6d733da6cb51158f049ad71529a6c
Olavo Neves <olavo9neves@gmail.com>
2025-03-12 15:32:03 -0300

    ex003

---
+++ python/exercicios/ex003/index.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor inteiro: '))
+n2 = float(input('Digite um valor real: '))
+s = n1 + n2
+print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
---
+++ python/exercicios/ex003/index01.py
@@ -0,0 +1,7 @@
+from jedi.inference.helpers import is_number
+
+n = float(input('Digite um valor real: '))
+print(n)
+
+teste = bool(input('Você gosta de macarrão? (true/false): '))
+print(teste)
