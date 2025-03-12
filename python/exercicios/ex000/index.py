8dc5b4d226259ca47ebb8330bc2f63cd50491c39
Olavo Neves <olavo9neves@gmail.com>
2025-03-12 14:25:15 -0300

    exs iniciais

---
+++ python/exercicios/ex000/index.py
@@ -0,0 +1,15 @@
+name = input('Digite seu nome: ')
+print('Bem vindo ao melhor lugar da internet ' + name)
+
+diaNascimento = int(input('Em que dia você nasceu? '))
+mesNascimento = input('Em que mês você nasceu? ')
+anoNascimento = int(input('Em qual ano você nasceu? '))
+
+print('Então você nasceu no dia ', diaNascimento, 'do ', mesNascimento, 'de ', anoNascimento)
+confirmacao = input('Certo ? (s/n)')
+
+print('Agora vamos fazer uma soma, por favor, ')
+firstNumber = int(input('Digite o primeiro número: '))
+secondNumber = int(input('Digite o segundo número: '))
+print('A soma é: ', firstNumber, secondNumber)
+
--- python/exercicios/ex001/index.py
+++ python/exercicios/ex001/index.py
@@ -1,12 +1,4 @@
-name = input('Digite seu nome: ')
-print('Bem vindo ao melhor lugar da internet ' + name)
+print('Hello word!')
 
-diaNascimento = int(input('Em que dia você nasceu? '))
-mesNascimento = input('Em que mês você nasceu? ')
-anoNascimento = int(input('Em qual ano você nasceu? '))
-
-print('Então você nasceu no dia ', diaNascimento, 'de ', mesNascimento, 'de ', anoNascimento, ', Certo?')
-
-firstNumber = int(input('Digite o primeiro número: '))
-secondNumber = int(input('Digite o segundo número: '))
-print('A soma é ', firstNumber + secondNumber)
+helloWord = input('Digite "Hello Word!": ')
+print('Parábens você digitou: ', helloWord)
