96a515e8065b3ce0f83675e6a9d9dd5776bd9ef8
Olavo Neves <olavo9neves@gmail.com>
2025-03-24 18:13:47 -0300

    Arrumando a questão do audio.mp3 dentro do codigo

--- python/desafios/desafio019.py
+++ python/desafios/desafio019.py
@@ -1,17 +1,26 @@
 # faça um programa que abra e reproduza o audio de um arquivo MP3
 import pygame
 
+# inicializa o pygame
+pygame.init()
+
 # Inicializa o mixer do pygame
 pygame.mixer.init()
 
 # Carrega o arquivo de música
-pygame.mixer.music.load('audio-encerramento.mp3') # ← Adicione o caminho do arquivo aqui
+try:
+    pygame.mixer.music.load('desafio019.mp3') # Adicione o caminho do arquivo aqui
+    print('Musica carregada com sucesso !')
 
-# Reproduz a música
-pygame.mixer.music.play()
+    # Reproduz a música
+    pygame.mixer.music.play()
+    print('Tocando musica...')
 
-# Para parar a música
-print('Pressione Enter para parar...') # Espera o usuário pressionar Enter
+    # Manter o programa rodando enquanto a musica toca
+    while pygame.mixer.music.get_busy():
+        continue
 
-# Parar a música
-pygame.mixer.music.stop()
+    print('Musica terminou sozinha !')
+
+finally:
+    pygame.quit()
