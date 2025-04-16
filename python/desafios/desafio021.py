caae11ac710ca1d83fc2bf8e88a151f161a3a5c1
Olavo Neves <olavo9neves@gmail.com>
2025-04-16 15:22:43 -0300

    arrumando interações com o usuario

--- python/desafios/desafio021.py
+++ python/desafios/desafio021.py
@@ -2,10 +2,10 @@ number = input("Digite um número: ")
 if number >= str(0) or number <= str(9999):
     print(f'Você digitou: {number}')
     print(f'''
-        unidade: {(number[3])}
-        dezena: {(number[2])}
-        centena: {(number[1])}
-        milhar: {(number[0])}
+        unidade: {int(number) // 1 % 10}
+        dezena: {int(number) // 10 % 10}
+        centena: {int(number) // 100 % 10}
+        milhar: {int(number) // 1000 % 10}
     ''')
 else:
     print('Você digitou um número negativo ou maior que o permitido no programa')
--- python/desafios/desafio024.py
+++ python/desafios/desafio024.py
@@ -1,5 +1,6 @@
-frase = input('Digite uma frase sobre algo que você gosta de fazer: ')
+frase = input('Digite uma frase sobre algo que você gosta de fazer: ').lower()
 
-print('Sua frase possui a letra a ', frase.find('a'), 'vezes')
-print('A letra a aparece primeiro na posição: ', frase.find('a'))
-print('A letra a aparece na ultima vez na posição: ', frase.rfind('a'))
+frase.strip()
+print('Sua frase possui a letra a ', frase.count('a'), 'vezes')
+print('A letra a aparece primeiro na posição: ', frase.find('a')+1)
+print('A letra a aparece na ultima vez na posição: ', frase.rfind('a')+1)
