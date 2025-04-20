adaf25c2f08edc9233f15a5a3a51c1edb380b013
Olavo Neves <olavo9neves@gmail.com>
2025-04-20 12:36:15 -0300

    estiliza exercicio

--- python/desafios/desafio026.py
+++ python/desafios/desafio026.py
@@ -2,12 +2,20 @@
 # falar se venceu ou não
 import random
 
+from time import sleep
+
 numeroSecreto = random.randint(1, 5)
 
+print("-=-" * 30)
 print("O computador pensou em um número entre 1 - 5")
+print("-=-" * 30)
 numeroUsuario = int(input("Tente acertar o número em um chute: "))
+print("Processando...")
+sleep(2.5)
 
 if numeroSecreto == numeroUsuario:
+    print(numeroSecreto, " = ", numeroUsuario)
     print("Você acertou o número secreto!")
 else:
+    print(numeroSecreto, " != ", numeroUsuario)
     print("Você não acertou o número secreto!")
